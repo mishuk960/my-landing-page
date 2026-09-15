8e2d4c8da9aa757256f5de78ba5620b2a6832732
mishuk960 <text2maruf@gmail.com>
2026-09-15 22:17:18 +0600

    Add affiliate configuration

---
+++ config.js
@@ -0,0 +1,30 @@
+const AFFILIATE_CONFIG = {
+  siteName: "My Landing Page",
+
+  offers: {
+    offer1: {
+      name: "Offer 1",
+      url: "https://example.com/offer1"
+    },
+
+    offer2: {
+      name: "Offer 2",
+      url: "https://example.com/offer2"
+    },
+
+    offer3: {
+      name: "Offer 3",
+      url: "https://example.com/offer3"
+    }
+  },
+
+  tracking: {
+    enabled: true,
+
+    utm: {
+      source: "affiliate",
+      medium: "landing-page",
+      campaign: "default"
+    }
+  }
+};
